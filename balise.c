a09fdfa42b5d70caba9973e016ae05b6adb1db90
Guillaume Lahaie <glahaie@gmail.com>
2012-12-02 17:55:36 -0500

    Premiere version de balise.c

---
+++ balise.c
@@ -0,0 +1,161 @@
+/* balise.c
+ * Par Guillaume Lahaie et Sylvain Labranche
+ *     LAHG04077707
+ *
+ * Date de remise: 18 décembre 2012
+ *
+ * Implémentation du fichier en-tête balise.h
+ *
+ */
+
+#include "balise.h"
+#include <ctype.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+struct balise {
+    Chaine nom;
+    Chaine attribut;
+    TypeBalise type;
+};
+
+
+//Vérifie si le malloc fonctionne bien. Retoune NULL dans ce cas.
+//Pour le moment je suppose que la Chaine nom passée contient encore
+//les < et >. Le nom doit contenir au moins un caractère.
+Balise baliseCree(Chaine nom) {
+
+    assert(nom != NULL && chaineLongueur(nom) > 2);
+    int i =1;
+    unsigned char c;
+    Balise b = (Balise)malloc(sizeof(struct balise));
+    char * ch = (char *)malloc(chaineLongueur(nom)*sizeof(char));
+
+    if(b == NULL || ch == NULL) {
+        return NULL;
+    }
+
+    ch = chaineValeur(nom);
+
+    //Obtenir le type, si possible
+    switch(ch[1]) {
+        case '!': b->type = COMMENTAIRES;
+                  break;
+        case '?': b->type = DIRECTIVE;
+                  break;
+        case '/': b->type = FIN;
+                  break;
+    }
+
+    if(b->type == COMMENTAIRES || b->type== DIRECTIVE) {
+        b->nom= b->attribut = NULL;
+        return b;
+    } else if (b->type == FIN) {
+        //Trouver le nom, vérifier qu'il y a seulement cela
+        b->nom = chaineCreeVide();
+        i = 2;
+        while(1) {
+            c = ch[i];
+            if(isalpha(c) || isdigit(c)) {
+                chaineAjoute(b->nom, c);
+            } else if (c == '>') {
+                //
+                break;
+            } else {
+                //Erreur
+            }
+            i++;
+        }
+    } else {
+        //C'est un début/ debutfin
+        //On trouve le nom - La vérification que le nom est conforme
+        //est à améliorer
+        b->nom = chaineCreeVide();
+        i = 1;
+        while(1) {
+            c = ch[i++];
+            if(isalpha(c) || isdigit(c)) {
+                chaineAjoute(b->nom, c);
+            } else if (isspace(c)) {
+                //nom fini
+                break;
+            } else {
+                //Erreur
+                fprintf(stderr, "Erreur - Mauvais nom de balise.\n");
+                exit(1);
+            }
+        }
+        
+        //On ajoute dans attribut maintenant et / ou type de balise
+        while(1) {
+            c = ch[i++];
+            if(c != '>' && c !='/') {
+                if(b->attribut == NULL) {
+                    b->attribut = chaineCreeVide();
+                }
+                chaineAjoute(b->attribut, c);
+            } else if (c == '>') {
+                b->type = DEBUT;
+                break;
+            } else if( c == '/') {
+                b->type = DEBUTFIN;
+                break;
+            }
+        }
+    }
+
+    return b;
+}
+
+
+Chaine baliseLitNom(Balise balise) {
+
+    assert(balise != NULL);
+    assert(baliseLitType(balise) != DIRECTIVE && baliseLitType(balise) != COMMENTAIRES);
+
+    Chaine retour = chaineCreeCopie(chaineValeur(balise->nom), chaineLongueur(balise->nom));
+    
+    if(retour == NULL) {
+        //Erreur
+    }
+
+    return retour;
+}
+
+TypeBalise baliseLitType(Balise balise) {
+    
+    assert(balise != NULL);
+
+    return balise->type;
+}
+ 
+Chaine baliseLitAttributs(Balise balise) {
+
+    assert(balise != NULL);
+    assert(baliseLitType(balise) != DIRECTIVE && baliseLitType(balise) != COMMENTAIRES);
+    Chaine att;
+
+    if(balise->attribut == NULL) {
+        return NULL;
+    } else {
+        att = chaineCreeCopie(chaineValeur(balise->attribut), chaineLongueur(balise->attribut));
+        if(att == NULL) {
+            //erreur
+        }
+    }
+    return att;
+}
+
+//Pour le moment je ne vérifie pas le type de la balise, car je crois 
+//qu'on peut devoir supprimer peu importe le type.
+void baliseSupprime(Balise balise) {
+
+    assert(balise != NULL);
+
+    free(balise->nom);
+    free(balise->attribut);
+    free(balise);
+}
+
--- chaine.c
+++ chaine.c
@@ -10,16 +10,106 @@
 
 #define PAS 10  //Pour agrandir la chaine si elle est trop petite
 #include "chaine.h"
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
+//Vérifier la différence entre int et size_t
 struct chaine {
-    void * tete;
-    int longueur;
+    char *s;
+    unsigned int longueur;
+    unsigned int longueur_max;
 };
 
 
-Chaine CreeVide() {
+Chaine chaineCreeVide() {
 
-    Chaine chaine;
-    chaine->tete = (char *)malloc(PAS*sizeof(char));
+    Chaine ch = (Chaine)malloc(sizeof(struct chaine));
 
-    //Verifie 
+    //Vérifier que les deux alloctions ont fonctionnés
+    ch->s = (char *)malloc(PAS*sizeof(char));
+
+    //Verifie que l'allocation a fonctionné
+    if (ch->s == NULL || ch == NULL)
+       return NULL;
+
+   //Sinon, on attribue les valeurs de base à la chaine
+   ch->longueur = 0;
+   ch->longueur_max = PAS;
+
+   return ch;
+}
+
+Chaine chaineCreeCopie(char * ch, unsigned n) {
+    
+    //Pourrait avoir un assert pour vérifier que ch se termine par \0
+    //Et que n est plus grand que 0.
+    //assert(n <= (sizeof(ch)/sizeof(char)));
+
+    Chaine chaine = (Chaine)malloc(sizeof(struct chaine));
+    chaine->s = (char *)malloc((n)*sizeof(char)); //+1 pour \0
+    
+    if(chaine->s == NULL || chaine == NULL) {
+        return NULL;
+    }
+
+    //On pourrait utiliser strncpy, mais le man semble dire que strcopy est meilleur.
+    strcpy(chaine->s, ch);
+
+    chaine->longueur = n-1;
+    chaine->longueur_max = n;
+
+    return chaine;
+}
+
+//Ici j'utilise une chaine temporaire pour éviter de perdre la chaine passer en entrée
+//si le realloc ne fonctionne pas.
+int chaineAjoute(Chaine chaine, unsigned char ch) {
+
+    //Assert que chaine est pas NULL
+    assert(chaine != NULL);
+    char * temp;
+
+    //Vérifie qu'il y a de la place dans la chaine pour un nouveau char.
+    if(chaine->longueur == chaine->longueur_max) {
+        temp = (char *)realloc(chaine->s, (chaine->longueur_max + PAS)*sizeof(char));
+        if(temp == NULL)
+            return 0;
+        chaine->longueur_max += PAS;
+    }
+
+    //Copier le caractère, avec déplacement du \0 à la fin.
+    temp[chaine->longueur++] = ch;
+    temp[chaine->longueur] = '\0';
+    free(chaine->s);
+    chaine->s = temp;
+    
+    return 1;
+}
+
+char * chaineValeur(Chaine chaine) {
+
+    assert(chaine != NULL);
+
+    char * ch = (char*)malloc(chaineLongueur(chaine)*sizeof(char));
+
+    //Quoi faire si malloc ne fonctionne pas? Est-ce qu'on retourne NULL?
+    strncpy(ch, chaine->s, chaineLongueur(chaine));
+
+    return ch;
+}
+
+unsigned chaineLongueur(Chaine chaine) {
+    assert(chaine != NULL);
+
+    return chaine->longueur;
+}
+
+void chaineSupprime(Chaine chaine) {
+    assert(chaine != NULL);
+
+    free(chaine->s);
+    free(chaine);
+    return;
+}
+
