f8578d2c3e4f044b68b3874c6496d0b4f4ed69fa
Guillaume Lahaie <glahaie@gmail.com>
2012-12-06 16:32:13 -0500

    Reste certaines vérifications d'allocation de mémoire à écrire et les commentaires, le reste semble bien fonctionner.

--- fichierBalises.c
+++ fichierBalises.c
@@ -21,14 +21,17 @@ struct fichierBalises {
     int position;
 };
 
-//Vérifier que nom_fichier n'est pas null?
 fichierBalises fichierBalisesOuvre(char * nom_fichier) {
 
     fichierBalises fib = (fichierBalises)malloc(sizeof(struct fichierBalises));
+    if(!fib) {
+        return NULL;
+    }
     
     fib->fic = fopen(nom_fichier, "r");
 
-    if(fib == NULL || fib->fic == NULL) {
+    if(!fib->fic) {
+        free(fib);
         return NULL;
     } else {
         fib->position = 0;
@@ -41,83 +44,74 @@ void fichierBalisesFerme(fichierBalises fichier) {
     assert(fichier != NULL && "fichier est NULL");
 
     fclose(fichier->fic);
-
     free(fichier);
+    return;
 }   
 
 Info fichierBalisesLit(fichierBalises fichier) {
 
     Info info;
     Chaine prochaine;
-    int c, finBoucle = 1;
-    int verif;
-    char *temp; //Pour éviter de perdre de la mémoire allouée
+    int prochainCar, 
+        verif;          
+    char *temp;         //Pour éviter de perdre de la mémoire allouée
 
+    info = (Info)malloc(sizeof(struct info));
+
+    //On s'assure que le fichier est à la dernière position lue
+    fseek(fichier->fic, fichier->position*sizeof(char), SEEK_SET);
+        
+    //On obtient le prochain caractère qui n'est pas un blanc.
     do {
+        prochainCar = fgetc(fichier->fic);
+        fichier->position++;
+    } while (isspace(prochainCar));
 
-        info = (Info)malloc(sizeof(struct info));
+    if(prochainCar == EOF) {
+        free(info);
+        return NULL;
+    }else if (prochainCar == '<') {
+        verif = '>';
+        info->type = BALISE;
+    } else {
+        verif = '<';
+        info->type = TEXTE;
+    }
 
-        //on s'assure que le fichier est au bon endroit
-        fseek(fichier->fic, fichier->position*sizeof(char), SEEK_SET);
+    prochaine = chaineCreeVide();
+    chaineAjoute(prochaine, (unsigned char)prochainCar);
         
-        //On obtient le prochain caractère qui n'est pas une whitespace.
-        do {
-            c = fgetc(fichier->fic);
+    //On rempli la chaine, jusq'au caractere verif ou jusqu'à la fin du fichier
+    do {    
+        prochainCar = fgetc(fichier->fic);
+        if((prochainCar == verif && info->type == BALISE) || prochainCar != verif) {
+            chaineAjoute(prochaine,(unsigned char)prochainCar);
             fichier->position++;
-        } while (isspace(c));
-        if(c == EOF) {
-            free(info);
-            return NULL;
-        }else if (c == '<') {
-            verif = '>';
-            info->type = BALISE;
-        } else {
-            verif = '<';
-            info->type = TEXTE;
         }
+    } while(prochainCar != verif && prochainCar != EOF);
 
-        prochaine = chaineCreeVide();
-        chaineAjoute(prochaine, (unsigned char)c);
         
-        //On remplie la chaine -- à vérifier pour balise, si EOF arrive avant la
-        //fin de la balise (>)
-        do {    
-            c = fgetc(fichier->fic);
-            if((c == verif && info->type == BALISE) || c != verif) {
-                chaineAjoute(prochaine,(unsigned char)c);
-                fichier->position++;
-            }
-        } while(c != verif && c != EOF);
+    if(info->type == TEXTE) {
 
-        //retourne TEXTE OU OBTIENT BALISE
-        if(info->type == TEXTE) {
-
-            temp = chaineValeur(prochaine);
-            if(!temp) {
-                //Malloc a échoué
-            } else {
-                info->contenu.texte = chaineCreeCopie(temp, chaineLongueur(prochaine));
-                free(temp);
-            }
-            chaineSupprimme(prochaine);
-            finBoucle = 1;
+        temp = chaineValeur(prochaine);
+        if(!temp) {
+            //Malloc a échoué
         } else {
-            //analyse de la balise
-            info->contenu.balise = baliseCree(prochaine);
-            chaineSupprimme(prochaine);
-            if(baliseLitType(info->contenu.balise) == COMMENTAIRES ||
-                        baliseLitType(info->contenu.balise) == DIRECTIVE) {
-                //On libère le struct info et balise et on recommence la boucle.
-                baliseSupprimme(info->contenu.balise);
-                free(info);
-                finBoucle = 0;
-            } else {
-                //C'est un bon type de balise à retourner. On libère prochaine             
-                //avant de retourner
-                finBoucle = 1;
-            }
+            info->contenu.texte = chaineCreeCopie(temp, chaineLongueur(prochaine));
+            free(temp);
         }
-    } while(!finBoucle);
+    } else {
+        //analyse de la balise
+        info->contenu.balise = baliseCree(prochaine);
+    }
+
+    chaineSupprimme(prochaine);
+    if(info->type == BALISE && (baliseLitType(info->contenu.balise) == COMMENTAIRES ||                       baliseLitType(info->contenu.balise) == DIRECTIVE)) {
+
+        baliseSupprimme(info->contenu.balise);
+        free(info);
+        info = fichierBalisesLit(fichier);                    
+    }
     return info;
 
 }
